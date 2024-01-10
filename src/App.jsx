a4071222107397d82c3f768000b49cd7768b3def
Oluwatobiloba Excel <tobyadeyy@gmail.com>
2024-01-10 14:22:19 +0100

    boiler plates + custom fonts added

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+const App = () => {
+  return (
+    <div>
+      <div>
+        logo here
+        <p>Talent Base</p>
+      </div>
+
+      <div>
+        <p className="font-DuplicateSans">source and hire top talents</p>
+      </div>
+
+      <div>
+        <p className="font-DuplicateSans text-red-300">custom font</p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
